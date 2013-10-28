59d4422099908e9009e8259489358f37b1be5803
olavnybo@gmail.com <olavnybo@gmail.com>
2013-10-28 21:33:05 +0100

    DbContext was missing from WebCustomUser project

---
+++ WebApplication/WebCustomUser/Models/CustomDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserModel;
+
+namespace WebCustomUser.Models
+{
+    public class CustomDbContext : IdentityDbContext<CustomUser>
+    {
+        public CustomDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
